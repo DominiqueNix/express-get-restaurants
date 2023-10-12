aeb6324e325c5f47d9ba7bcc62cb8389aa889cba
DominiqueNix <dominiquenix@google.com>
2023-10-12 13:59:59 -0500

    added the /restaurants endpoint

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
-
-
+app.get('/restaurants', async (req, res) => {
+    let allRestaurants = await Restaurant.findAll();
+    res.json(allRestaurants)
+})
 
 module.exports = app;
