d00f1c8f11c9fc1a7698f7c25c0b64be52ef99fa
DominiqueNix <dominiquenix@google.com>
2023-10-16 09:43:16 -0500

    created post,put, and delete routes for a restaurant

--- src/app.js
+++ src/app.js
@@ -15,4 +15,26 @@ app.get('/restaurants/:id', async(req, res) => {
     res.json(oneRestaurant)
 })
 
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/restaurants', async(req,res) => {
+    // let data = req.body;
+    let newRestaurant = await Restaurant.create(req.body)
+    console.log(newRestaurant)
+    res.json(newRestaurant)
+})
+
+app.put('/restaurant/:id', async(req,res) => {
+    let updatedRestaurant = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(updatedRestaurant)
+})
+
+app.delete('/restaurant/:id', async(req,res) => {
+    let deletedRestaurant = await Restaurant.destroy({
+        where: {id: req.params.id}
+    });
+    res.json(deletedRestaurant)
+})
+
 module.exports = app;
